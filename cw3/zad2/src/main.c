84c895b9603a9925ae2a4165fb008ce43d47da37
Tomasz Odrzywolek <tomasz@chainfire.co.uk>
2019-01-04 01:34:30 +0100

    refactor and fix compilation errors in functions

--- cw3/zad2/src/main.c
+++ cw3/zad2/src/main.c
@@ -1,33 +1,43 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <errno.h>
-//#include <limits.h>
-#include <sys/types>
-
-void validate_arguments_number(int number_of_arguments);
-FILE *open_input_file(char *filename);
-void read_line(char *line);
-void err_sys(const char *x);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 struct Command
 {
     char *program_name;
     char *args[10];
-}
+};
 
-int
-main(int argc, char const *argv[])
+void validate_arguments_number(int number_of_arguments);
+FILE *open_input_file(char const *filename);
+void read_line(char *line);
+void read_line_2(char *line, struct Command *command);
+int execute_in_separate_process(struct Command *command);
+void err_sys(const char *x);
+
+int main(int argc, char const *argv[])
 {
     validate_arguments_number(argc);
 
-    FILE *fp = open_input_file("arguments.txt");
+    FILE *fp = open_input_file(argv[1]);
 
     char line[255];
     struct Command command;
-    while (fgets(line, sizeof(line), fp) == NULL)
+    int execution_result = 0, line_number = 1;
+    while (fgets(line, sizeof(line), fp) != NULL)
     {
-        read_line(fp);
-        execute_in_separate_process(&command);
+        read_line_2(line, &command);
+        execution_result = execute_in_separate_process(&command);
+        if (execution_result < 0)
+        {
+            printf("execution failure on line %d. Stopping program.\n", line_number);
+            fclose(fp);
+            exit(-1);
+        }
+        line_number++;
     }
 
     fclose(fp);
@@ -45,11 +55,11 @@ void validate_arguments_number(int number_of_arguments)
     }
 }
 
-FILE *open_input_file(char *filename)
+FILE *open_input_file(char const *filename)
 {
     FILE *fp;
     fp = fopen(filename, "r");
-    if (!in)
+    if (!fp)
     {
         printf("Failed to open text file\n");
         exit(1);
@@ -57,43 +67,38 @@ FILE *open_input_file(char *filename)
     return fp;
 }
 
-void read_line(char *line)
-{
-    for (char *p = strtok(line, " "); p != NULL; p = strtok(NULL, " "))
-    {
-        puts(p);
-    }
-}
-
 void read_line_2(char *line, struct Command *command)
 {
     char *p = strtok(line, " ");
-    printf("%s", p);
     command->program_name = p;
     int i = 0;
 
-    for (*p = strtok(line, " "); p != NULL; p = strtok(NULL, " "))
+    for (; p != NULL; p = strtok(NULL, " "))
     {
-        printf("%s", p);
         command->args[i] = p;
         i++;
     }
+    // strip newline from the end
+    command->args[i - 1][strcspn(command->args[i - 1], "\n")] = 0;
     command->args[i] = (char *)0;
 }
 
-void execute_in_separate_process(struct Command *command)
+int execute_in_separate_process(struct Command *command)
 {
-    pit_t pid;
+    pid_t pid;
 
     if ((pid = fork()) < 0)
         err_sys("fork_error");
     else if (pid == 0)
     {
-        if (execvp(command->program_name, command->program_name, command->args) < 0)
-            err_sys("execvp error");
+        int execution_result = execvp(command->program_name, command->args);
+        if (execution_result < 0)
+            return execution_result;
     }
     if (waitpid(pid, NULL, 0) < 0)
         err_sys("wait error");
+
+    return 0;
 }
 
 void err_sys(const char *x)
