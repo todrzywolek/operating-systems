6970e2bda4b61b3d377d456a59b6d5ddc1edcac7
Tomasz Odrzywolek <tomeko92@gmail.com>
2019-01-08 01:00:56 +0100

    add makefile and start script

---
+++ zad2/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+make
+make clean
+
+# L - num of signals parent should send to child
+L=4
+
+# Type - signal send mode
+#   1 - sends SIGUSR1 and SIGUSR2 to child process using 'kill' asynchronously
+#   2 - sends SIGUSR1 and SIGUSR2 to child process using 'kill' after receiveing confirmation from child
+#   3 - sends SIGRTMIN and (SIGRTMIN+1) to child process using 'kill' after receiveing confirmation from child
+Type=1
+
+./parent $L $Type
